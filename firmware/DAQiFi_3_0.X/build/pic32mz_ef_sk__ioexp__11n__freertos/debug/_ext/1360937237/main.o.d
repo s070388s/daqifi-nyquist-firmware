1898d46ae43ee7d0f0f8f947ffc794e0b873de47
Daniel Miller <dmiller@daqifi.com>
2018-05-25 16:54:21 +0000

    Remove intermediate build directories

--- firmware/DAQiFi_3_0.X/build/pic32mz_ef_sk__ioexp__11n__freertos/debug/_ext/1360937237/main.o.d
+++
@@ -1,4 +0,0 @@
-build/pic32mz_ef_sk__ioexp__11n__freertos/debug/_ext/1360937237/main.o:  \
- ../src/main.c  \
-../../../../../framework/system/common/sys_module.h  \
- ../../../../../framework/system/common/sys_common.h 
--- firmware/DAQiFi_3_0.X/build/pic32mz_ef_sk__ioexp__11n__freertos/debug/_ext/1920085922/AInSample.o.d
+++
@@ -1,6 +0,0 @@
-build/pic32mz_ef_sk__ioexp__11n__freertos/debug/_ext/1920085922/AInSample.o:  \
- ../src/state/data/AInSample.c  \
-../src/state/data/AInSample.h  \
- ../src/Util/ArrayWrapper.h  \
-../src/Util/HeapList.h  \
- ../src/Util/LockProvider.h 
