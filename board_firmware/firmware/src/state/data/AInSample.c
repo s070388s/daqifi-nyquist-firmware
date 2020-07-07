d6872fcff29f90aeab550c4f47360014e39fc3f6
Javier Longares Abaiz <contacto@javierlongares.com>
2020-07-07 21:03:04 +0200

    Porting of AInSample.c and DIOSample.c to use Queues in place of HeapList

--- board_firmware/firmware/src/state/data/AInSample.c
+++ board_firmware/firmware/src/state/data/AInSample.c
@@ -1,5 +1,13 @@
+/*! @file AInSample.c
+ * @brief File with the implementation of the functionality to send and receive
+ * messages with the data coming from Analog Inputs.
+ * @author Javier Longares Abaiz - When Technology becomes art.
+ * @web www.javierlongares.com
+ */
+
 #include "AInSample.h"
 
+#if USE_HEAPLIST == 1
 void AInSampleList_Initialize(AInSampleList* list, size_t maxSize, bool dropOnOverflow, const LockProvider* lockPrototype)
 {
     HeapList_Initialize(&list->List, maxSize, dropOnOverflow, lockPrototype);
@@ -34,3 +42,107 @@ bool AInSampleList_IsEmpty(AInSampleList* list)
 {
     return HeapList_IsEmpty(&list->List);
 }
+#else
+#include "FreeRTOS.h"
+#include "queue.h"
+
+//! Ticks to wait for QUEUE OPERATIONS
+#define AINSAMPLE_QUEUE_TICKS_TO_WAIT               1000
+
+//! Queue handler for Analog inputs
+static QueueHandle_t analogInputsQueue;
+//! Size of the queue, in number of items
+static uint32_t queueSize = 0;
+
+void AInSampleList_Initialize( \
+                            AInSampleList* list, \
+                            size_t maxSize, \
+                            bool dropOnOverflow, \
+                            const LockProvider* lockPrototype){
+    
+    (void)list;
+    (void)maxSize;
+    (void)dropOnOverflow;
+    (void)lockPrototype;
+    
+    queueSize = maxSize;
+    analogInputsQueue = xQueueCreate( maxSize, sizeof(AInSample) );
+}
+
+void AInSampleList_Destroy(AInSampleList* list)
+{
+    (void)list;
+    
+    vQueueDelete( analogInputsQueue );
+}
+
+bool AInSampleList_PushBack(AInSampleList* list, const AInSample* data){
+    BaseType_t queueResult;
+    
+    if( data == NULL ){
+        return false;
+    }
+    
+    (void)list;
+    
+    queueResult = xQueueSend( \
+                    analogInputsQueue, \
+                    data, \
+                    AINSAMPLE_QUEUE_TICKS_TO_WAIT );
+    return ( queueResult == pdTRUE ) ? true : false; 
+}
+
+bool AInSampleList_PopFront(AInSampleList* list, AInSample* data)
+{
+    BaseType_t queueResult;
+    
+    (void) list;
+    
+    if( data == NULL ){
+        return false;
+    }
+    
+    queueResult = xQueueReceive( \
+                    analogInputsQueue, \
+                    data, \
+                    AINSAMPLE_QUEUE_TICKS_TO_WAIT );
+    return ( queueResult == pdTRUE ) ? true : false; 
+}
+
+bool AInSampleList_PeekFront(AInSampleList* list, AInSample* data)
+{
+    BaseType_t queueResult;
+    
+    (void)list;
+    
+    if( data == NULL ){
+        return false;
+    }
+    
+    queueResult = xQueuePeek( \
+                    analogInputsQueue, \
+                    data, \
+                    AINSAMPLE_QUEUE_TICKS_TO_WAIT );
+    return ( queueResult == pdTRUE ) ? true : false; 
+}
+
+size_t AInSampleList_Size(AInSampleList* list)
+{
+    (void)list;
+    
+    if( queueSize == 0 ){
+        return 0;
+    }
+    return (queueSize - uxQueueSpacesAvailable( analogInputsQueue ) );
+}
+
+bool AInSampleList_IsEmpty(AInSampleList* list)
+{
+    (void)list;
+    
+    if( AInSampleList_Size(NULL) == queueSize ){
+        return true;
+    }
+    return false;
+}
+#endif /* USE_HEAP_LIST */
--- board_firmware/firmware/src/state/data/DIOSample.c
+++ board_firmware/firmware/src/state/data/DIOSample.c
@@ -1,5 +1,6 @@
 #include "DIOSample.h"
 
+#if USE_HEAPLIST == 1
 void DIOSampleList_Initialize(DIOSampleList* list, size_t maxSize, bool dropOnOverflow, const LockProvider* lockPrototype)
 {
     HeapList_Initialize(&list->List, maxSize, dropOnOverflow, lockPrototype);
@@ -29,3 +30,108 @@ bool DIOSampleList_IsEmpty(DIOSampleList* list)
 {
     return HeapList_IsEmpty(&list->List);
 }
+
+#else
+#include "FreeRTOS.h"
+#include "queue.h"
+
+//! Ticks to wait for QUEUE OPERATIONS
+#define DIOSAMPLE_QUEUE_TICKS_TO_WAIT               1000
+
+//! Queue handler for DIO values
+static QueueHandle_t DIOQueue;
+//! Size of the queue, in number of items
+static uint32_t queueSize = 0;
+
+void DIOSampleList_Initialize( \
+                            DIOSampleList* list, \
+                            size_t maxSize, \
+                            bool dropOnOverflow, \
+                            const LockProvider* lockPrototype){
+    
+    (void)list;
+    (void)maxSize;
+    (void)dropOnOverflow;
+    (void)lockPrototype;
+    
+    queueSize = maxSize;
+    DIOQueue = xQueueCreate( maxSize, sizeof(DIOSample) );
+}
+
+void DIOSampleList_Destroy(DIOSampleList* list)
+{
+    (void)list;
+    
+    vQueueDelete( DIOQueue );
+}
+
+bool DIOSampleList_PushBack(DIOSampleList* list, const DIOSample* data){
+    BaseType_t queueResult;
+    
+    if( data == NULL ){
+        return false;
+    }
+    
+    (void)list;
+    
+    queueResult = xQueueSend( \
+                    DIOQueue, \
+                    data, \
+                    DIOSAMPLE_QUEUE_TICKS_TO_WAIT );
+    return ( queueResult == pdTRUE ) ? true : false; 
+}
+
+bool DIOSampleList_PopFront(DIOSampleList* list, DIOSample* data)
+{
+    BaseType_t queueResult;
+    
+    (void) list;
+    
+    if( data == NULL ){
+        return false;
+    }
+    
+    queueResult = xQueueReceive( \
+                    DIOQueue, \
+                    data, \
+                    DIOSAMPLE_QUEUE_TICKS_TO_WAIT );
+    return ( queueResult == pdTRUE ) ? true : false; 
+}
+
+bool DIOSampleList_PeekFront(DIOSampleList* list, DIOSample* data)
+{
+    BaseType_t queueResult;
+    
+    (void)list;
+    
+    if( data == NULL ){
+        return false;
+    }
+    
+    queueResult = xQueuePeek( \
+                    DIOQueue, \
+                    data, \
+                    DIOSAMPLE_QUEUE_TICKS_TO_WAIT );
+    return ( queueResult == pdTRUE ) ? true : false; 
+}
+
+size_t DIOSampleList_Size(DIOSampleList* list)
+{
+    (void)list;
+    
+    if( queueSize == 0 ){
+        return 0;
+    }
+    return (queueSize - uxQueueSpacesAvailable( DIOQueue ) );
+}
+
+bool DIOSampleList_IsEmpty(DIOSampleList* list)
+{
+    (void)list;
+    
+    if( DIOSampleList_Size(NULL) == queueSize ){
+        return true;
+    }
+    return false;
+}
+#endif /* USE_HEAP_LIST */
